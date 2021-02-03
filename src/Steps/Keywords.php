3d84ffc7f500997cd91f1ce245d11c681dd6fdee
Andrey Helldar <helldar@ai-rus.com>
2021-02-03 16:53:32 +0300

    Additional processing of keywords

--- src/Steps/Keywords.php
+++ src/Steps/Keywords.php
@@ -12,4 +12,11 @@ final class Keywords extends BaseStep
     {
         return $this->getIO()->askAndValidate($this->question(), static fn($value) => trim($value));
     }
+
+    protected function post($result): array
+    {
+        sort($result);
+
+        return array_values(array_unique(array_filter($result)));
+    }
 }

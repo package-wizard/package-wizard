4d25a88b2ccfa731b5de5f1c7744c5743afbf10d
andrey-helldar <10347617+andrey-helldar@users.noreply.github.com>
2025-03-13 20:17:00 +0000

    🦋 The code style has been fixed

--- config/app.php
+++ config/app.php
@@ -1,7 +1,8 @@
 <?php
 
-return [
+declare(strict_types=1);
 
+return [
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -56,5 +57,4 @@ return [
     'providers' => [
         App\Providers\AppServiceProvider::class,
     ],
-
 ];

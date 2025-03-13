b1e4556bfa18a06710654146b9aa43690fb7a07f
andrey-helldar <10347617+andrey-helldar@users.noreply.github.com>
2025-03-13 20:28:07 +0000

    🦋 The code style has been fixed

--- app/Commands/InspireCommand.php
+++ app/Commands/InspireCommand.php
@@ -18,13 +18,13 @@ class InspireCommand extends Command
     {
         render(
             <<<'HTML'
-            <div class="py-1 ml-2">
-                <div class="px-1 bg-blue-300 text-black">Laravel Zero</div>
-                <em class="ml-1">
-                  Simplicity is the ultimate sophistication.
-                </em>
-            </div>
-        HTML
+                    <div class="py-1 ml-2">
+                        <div class="px-1 bg-blue-300 text-black">Laravel Zero</div>
+                        <em class="ml-1">
+                          Simplicity is the ultimate sophistication.
+                        </em>
+                    </div>
+                HTML
         );
     }
 }

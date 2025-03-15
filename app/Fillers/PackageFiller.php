8270eca6289c56eed7bfe35cc6814079326275cd
Andrey Helldar <helldar@dragon-code.pro>
2025-03-15 14:18:37 +0300

    Added conclusion of the preset of available projects

--- app/Fillers/PackageFiller.php
+++ app/Fillers/PackageFiller.php
@@ -5,22 +5,45 @@ declare(strict_types=1);
 namespace PackageWizard\Installer\Fillers;
 
 use PackageWizard\Installer\Integrations\Packagist;
+use PackageWizard\Installer\Services\ProjectService;
 
 use function Laravel\Prompts\search;
+use function Laravel\Prompts\select;
 
 /** @method static make() */
 class PackageFiller extends Filler
 {
     public function __construct(
         protected Packagist $packagist,
+        protected ProjectService $project,
     ) {}
 
     public function get(): string
     {
+        if (($name = $this->local()) !== ProjectService::Search) {
+            return $name;
+        }
+
+        return $this->packagist();
+    }
+
+    protected function local(): ?string
+    {
+        return select(
+            label   : 'Select a project:',
+            options : $this->project->list(),
+            scroll  : 10,
+            required: 'The project name is required.',
+        );
+    }
+
+    protected function packagist(): string
+    {
         return search(
             label      : 'What is the name of project?',
             options    : fn (string $value) => $this->packagist->search($value),
-            placeholder: 'E.g. monolog/monolog'
+            placeholder: 'E.g. monolog/monolog',
+            required   : 'The project name is required.',
         );
     }
 }

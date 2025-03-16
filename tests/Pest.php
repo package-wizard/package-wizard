35edaba421feed1deb4ca00f44e4a916a90a0024
Andrey Helldar <helldar@dragon-code.pro>
2025-03-16 18:58:22 +0300

    Added protection against sending real questions during testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Http;
 use PHPUnit\Framework\TestCase;
 
-uses(TestCase::class)->in('Feature');
+uses(TestCase::class)
+    ->in('Feature')
+    ->beforeEach(static function () {
+        Http::preventStrayRequests();
+    });

8d8e63e991315f10531299bfb541fbe859b83b9b
Andrey Helldar <helldar@dragon-code.pro>
2025-03-18 02:46:09 +0300

    Rename `comparator` with `operator`

--- app/Enums/ConditionOperatorEnum.php
+++ app/Enums/ConditionOperatorEnum.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PackageWizard\Installer\Enums;
 
-enum ComparatorEnum: string
+enum ConditionOperatorEnum: string
 {
     case LessThan             = '<';
     case LessThanOrEqualTo    = '<=';

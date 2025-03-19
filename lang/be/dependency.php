6d912e4f8457c51fca41bb156ea74baceaf3ea41
Andrey Helldar <helldar@dragon-code.pro>
2025-03-19 13:59:57 +0300

    Added Ukrainian and Belarusian localizations

---
+++ lang/be/dependency.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'install'     => '[:name] Усталяванне залежнасцей...',
+    'install_dev' => '[:name] Усталяванне dev залежнасцей...',
+    'remove'      => '[:name] Выдаленне залежнасцей...',
+    'remove_dev'  => '[:name] Выдаленне dev залежнасцей...',
+];
